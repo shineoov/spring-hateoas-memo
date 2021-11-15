475d95eaea5b218d39603e8b3c9d495a983eee40
shineoov <shineoov88@gmail.com>
2021-11-15 12:16:12 +0900

    project initialize

---
+++ src/main/java/me/springhateoasmemo/SpringHateoasMemoApplication.java
@@ -0,0 +1,13 @@
+package me.springhateoasmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringHateoasMemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringHateoasMemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/springhateoasmemo/SampleTest.java
@@ -0,0 +1,14 @@
+package me.springhateoasmemo;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.hateoas.IanaLinkRelations;
+import org.springframework.hateoas.Link;
+
+import static org.assertj.core.api.Assertions.*;
+
+
+public class SampleTest {
+
+
+}
